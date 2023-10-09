1d92594aa4a09b74dd2c1950282e2c5112719a64
ertyp <ertyperty24@gmail.com>
2023-10-09 21:27:57 +0200

    added the freetype and other needed libraries by the SDL_ttf

---
+++ build-VS/CMakeFiles/CMakeScratch/TryCompile-uku82k/CMakeFiles/cmTC_241fd.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/ertyp/CLionProjects/XSUI-lib/build-VS/CMakeFiles/CMakeScratch/TryCompile-uku82k/src.c" "CMakeFiles/cmTC_241fd.dir/src.c.obj" "msvc" "CMakeFiles/cmTC_241fd.dir/src.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build-VS/CMakeFiles/CMakeScratch/TryCompile-uku82k/CMakeFiles/cmTC_241fd.dir/depend.make
@@ -0,0 +1,2 @@
+﻿# Empty dependencies file for cmTC_241fd.
+# This may be replaced when dependencies are built.
